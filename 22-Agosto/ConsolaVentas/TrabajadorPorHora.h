e618bddbcbfbc81fc1eb99577563d4e57672f060
Alejandro Solares <josue.alejandro.solares@gmail.com>
2020-08-22 03:40:56 -0600

    TOCANDO EL MAIN

--- 22-Agosto/ConsolaVentas/TrabajadorPorHora.h
+++ 22-Agosto/ConsolaVentas/TrabajadorPorHora.h
@@ -7,13 +7,13 @@ private:
     float salarioPorHora;
 public:
     TrabajadorPorHora(){
-        this->numHT =0;
-        this->salarioPorHora =0;
+        this->numHT = 0;
+        this->salarioPorHora = 0;
     }
     TrabajadorPorHora(int idP, string nP, string aP, int nHT, float sPH) : Trabajador(idP,nP,aP){
         this->numHT = nHT;
         this->salarioPorHora = sPH;
-        this->sueldo = nHT*sPH;
+        this->sueldo = nHT * sPH;
         this->descuentoIsr = this->sueldo * 0.1;
         this->totalDescuentos = this->descuentoIsr;
         this->totalPagar = this->sueldo - this->totalDescuentos;
